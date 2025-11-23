bccef86a98f080e4b238361ee3a04b69cb7b4f79
Tanya Agrawal <tanyaagrawal256@gmail.com>
2025-11-23 22:30:34 +0530

    Update plots.py

--- src/plots.py
+++ src/plots.py
@@ -1,1 +1,7 @@
-#placeholder
+import matplotlib.pyplot as plt
+
+
+def save_current_plot(path: str) -> None:
+    """Save the current Matplotlib figure to the given path."""
+    plt.savefig(path, dpi=300, bbox_inches="tight")
+
